6eb87e58133878c63820f1baf518268cae2f4556
Manoj23355 <manoj.funny.varma@gmail.com>
2025-03-25 21:31:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,29 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type = string
+  }
 
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type = string
+  }
+
+variable "subnets_cidr" {
+  description = "list of subnets cidr"
+  type = list(string)
+ }
+
+variable "instance_type" {
+  description = "ec2 instance type"
+  type = string
+ }
+
+variable "ami_id" {
+  description = "enter the ami id"  
+  type = string
+  }
+
+  variable "allowed_ports" {
+    description = "enter the port numbers"
+    type = list(number)
+    }
